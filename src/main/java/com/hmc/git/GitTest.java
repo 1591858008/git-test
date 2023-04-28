65d66716462ad0d4f529309975022706b985aca3
hmc <1591858008@qq.com>
2023-04-28 21:13:46 +0800

    first commit

---
+++ src/main/java/com/hmc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hmc.git;
+
+/**
+ * @author hmc
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, Git");
+    }
+}

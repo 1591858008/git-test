7e55bf7acf5b7de41d5798bb966fba09c85bde42
hmc <1591858008@qq.com>
2023-04-28 21:32:56 +0800

    master commit2

--- src/main/java/com/hmc/git/GitTest.java
+++ src/main/java/com/hmc/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, Git！ 我改了两次");
         System.out.println("hot-fix");
+        System.out.println("master test");
     }
 }

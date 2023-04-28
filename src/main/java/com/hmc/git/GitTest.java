77f01f3b89e277ca69d5818e5000a76e1c870c00
hmc <1591858008@qq.com>
2023-04-28 21:19:04 +0800

    third commit

--- src/main/java/com/hmc/git/GitTest.java
+++ src/main/java/com/hmc/git/GitTest.java
@@ -6,6 +6,6 @@ package com.hmc.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello, Git！ 我改了一次");
+        System.out.println("hello, Git！ 我改了两次");
     }
 }
